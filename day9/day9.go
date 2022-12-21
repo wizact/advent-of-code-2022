fbe1b2905b6201a0ae412097b8f064610f2fc0b1
Amir Mohtasebi <amir.mohtasebi@gmail.com>
2022-12-21 10:40:17 +0000

    refactor day 9 and add remote debugging config

--- day9/day9.go
+++ day9/day9.go
@@ -16,43 +16,60 @@ const RIGHT string = "R"
 
 func Day9() {
 	ins := getInstructions()
-	drawMap(ins)
 	trackMovement(ins)
 }
 
-type Postion struct {
+type Position struct {
 	x int
 	y int
 }
 
 func trackMovement(ins []Instruction) {
-	arr := []Postion{}
-	headPosition := Postion{x: 0, y: 0}
-	tailPosition := Postion{x: 0, y: 0}
+	noOfKnots := 2
+	arr := []Position{}
+
+	knots := make(map[int]Position, noOfKnots)
+	for i := 0; i < noOfKnots; i++ {
+		knots[i] = Position{x: 0, y: 0}
+	}
 
 	for i := 0; i < len(ins); i++ {
-		arr = move(ins[i], &headPosition, &tailPosition, arr)
+		arr = move(ins[i], knots, arr)
 	}
 
 	fmt.Println("Number of unique positions tail has been at least once:", len(arr))
 }
 
-func move(ins Instruction, hp *Postion, tp *Postion, arr []Postion) []Postion {
-	return stepThroughInstruction(ins, hp, tp, arr)
+func move(ins Instruction, knots map[int]Position, arr []Position) []Position {
+	return stepThroughInstruction(ins, knots, arr)
 }
 
-func stepThroughInstruction(ins Instruction, hp *Postion, tp *Postion, arr []Postion) []Postion {
+func stepThroughInstruction(ins Instruction, knots map[int]Position, arr []Position) []Position {
 	for i := 0; i < ins.steps; i++ {
-		adjustPosition(Instruction{direction: ins.direction, steps: 1}, hp)
-		calcTailRelativePostion(hp, tp, ins)
-		if !tailAlreadyBeenInPosition(arr, tp) {
-			arr = append(arr, Postion{x: tp.x, y: tp.y})
+		head := knots[0]
+		adjustPosition(Instruction{direction: ins.direction, steps: 1}, &head)
+		knots[0] = head
+		for j := 0; j < len(knots)-1; j++ {
+
+			k1 := knots[j]
+			k2 := knots[j+1]
+
+			calcTailRelativePostion(&k1, &k2, ins)
+
+			if j+2 == len(knots) {
+				if !tailAlreadyBeenInPosition(arr, &k2) {
+					arr = append(arr, Position{x: k2.x, y: k2.y})
+				}
+			}
+
+			knots[j+1] = k2
 		}
 	}
+
 	return arr
 }
 
-func adjustPosition(ins Instruction, p *Postion) {
+func adjustPosition(ins Instruction, p *Position) {
 	switch ins.direction {
 	case UP:
 		p.y = p.y + ins.steps
@@ -65,28 +82,14 @@ func adjustPosition(ins Instruction, p *Postion) {
 	}
 }
 
-func calcTailRelativePostion(hp, tp *Postion, ins Instruction) {
+func calcTailRelativePostion(hp, tp *Position, ins Instruction) {
 	newTailInstruction := &Instruction{}
 	xDistance := hp.x - tp.x
 	yDistance := hp.y - tp.y
 	absXDistance := math.Abs(float64(xDistance))
 	absYDistance := math.Abs(float64(yDistance))
 
-	if xDistance == 0 && yDistance == 0 {
-		// Overlapping, no adjustment is required
-		return
-	}
-
-	touchingX := absXDistance == 1
-	touchingY := absYDistance == 1
-
-	if ((touchingX || touchingY) && !(touchingX && touchingY)) && absXDistance < 2 && absYDistance < 2 {
-		// Touching adjacent, no adjustment is required
-		return
-	}
-
-	if touchingX && touchingY {
-		// Touching diagonally, no adjustment is required
+	if absXDistance < 2 && absYDistance < 2 {
 		return
 	}
 
@@ -115,54 +118,26 @@ func calcTailRelativePostion(hp, tp *Postion, ins Instruction) {
 			}
 			newTailInstruction.steps = int(absYDistance) - 1
 		}
-		adjustPosition(*newTailInstruction, tp)
-		return
-	}
-
-	// Distant diagonally. Adjustment is required. Move tail to the same direction as head has moved by x steps
-	if ins.direction == UP || ins.direction == DOWN {
-		newTailInstruction.steps = int(math.Abs(float64(hp.y-tp.y)) - 1)
-		newTailInstruction.direction = ins.direction
-		tp.x = hp.x
 	} else {
-		newTailInstruction.steps = int(math.Abs(float64(hp.x-tp.x)) - 1)
+		// Distant diagonally. Adjustment is required. Move tail to the same direction as head has moved by x steps
+		newTailInstruction.steps = 1
 		newTailInstruction.direction = ins.direction
-		tp.y = hp.y
+		if ins.direction == UP || ins.direction == DOWN {
+			tp.x = hp.x
+		} else {
+			tp.y = hp.y
+		}
 	}
-
 	adjustPosition(*newTailInstruction, tp)
 }
 
-func tailAlreadyBeenInPosition(arr []Postion, tp *Postion) bool {
+func tailAlreadyBeenInPosition(arr []Position, tp *Position) bool {
 	for _, v := range arr {
 		if v.x == tp.x && v.y == tp.y {
 			return true
 		}
 	}
 	return false
-}
-
-func drawMap(ins []Instruction) {
-	var u, d, l, r int
-
-	for i := 0; i < len(ins); i++ {
-		switch ins[i].direction {
-		case UP:
-			u = u + ins[i].steps
-		case DOWN:
-			d = d - ins[i].steps
-		case LEFT:
-			l = l - ins[i].steps
-		case RIGHT:
-			r = r + ins[i].steps
-		}
-	}
-
-	fmt.Println("Total movement up:", u, "down:", d, "left:", l, "right:", r)
-
-	x := r + l
-	y := u + d
-	fmt.Println("Total movement of head horizontally:", x, "and vertically", y)
 }
 
 func getInstructions() []Instruction {
